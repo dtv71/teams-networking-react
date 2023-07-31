580066e2d119aec92a66e486c4394372ade3fe18
dtv71 <developer@codecloud.ro>
2023-07-31 20:31:40 +0300

    change tsconfig to be less restrictive

---
+++ src/examples.ts
@@ -0,0 +1,14 @@
+let firstName = 'yes';
+//firstName = false;
+
+function getConf() {
+    return {
+        name: "my conf",
+        x: 123,
+        ready: true
+    };
+}
+
+const conf = getConf();
+//console.info(conf.nume)
+export default null
