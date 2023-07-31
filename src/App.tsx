b5340c417959fcda1c67ad401025c7f0c3043145
dtv71 <developer@codecloud.ro>
2023-07-31 21:03:08 +0300

    add AppFooter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 //import logo from './logo.svg';
-import './style.css';
-import logo from  './images/network-team-icon.png'
+import logo from "./images/network-team-icon.png";
+import "./style.css";
 
 function App_old() {
   return (
@@ -11,12 +11,7 @@ function App_old() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
@@ -24,21 +19,47 @@ function App_old() {
   );
 }
 
-function AppHeader(){
-  return <header>
-  <div id="header-wrapper">
-    <div id="my-picture">
-      <img src={logo} alt="poza" width="100" />
-    </div>
-    <div id="header-info">
-      <h1>Teams Networking</h1>
-      <h2 id="job-title">CRUD operations (CREATE, READ, UPDATE, DELETE)</h2>
-    </div>
-  </div>
-</header>
+function AppHeader() {
+  return (
+    <header>
+      <div id="header-wrapper">
+        <div id="my-picture">
+          <img src={logo} alt="poza" width="100" />
+        </div>
+        <div id="header-info">
+          <h1>Teams Networking</h1>
+          <h2 id="job-title">CRUD operations (CREATE, READ, UPDATE, DELETE)</h2>
+        </div>
+      </div>
+    </header>
+  );
+}
+function ContentWrapper() {
+  return <div id="main"> Todo ...</div>;
 }
 
-function App(){
-  return <AppHeader />
+function AppFooter() {
+  return (
+    <footer>
+      <a className="item" href="https://github.com/dtv71/teams-networking-test/tree/main" target="_blank">
+        <span>👩‍💻 </span>
+        <span>Source code</span>
+      </a>
+      <span className="item">|</span>
+      <a className="item" href="https://github.com/nmatei/node-api" target="_blank">
+        <span>📃</span>
+        <span>API docs</span>
+      </a>
+    </footer>
+  );
+}
+function App() {
+  return (
+    <>
+      <AppHeader />
+      <ContentWrapper />
+      <AppFooter />
+    </>
+  );
 }
 export default App;
